73f4e3cda75e72538345de8d8e4fc43f87e15b61
galinagumenyuk <gymenyuk.galina@gmail.com>
2021-10-06 19:15:17 +0300

    upd

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import fetchCountries from "./fetchCountries";
+// import fetchCountries from "./fetchCountries";
 import * as _ from "lodash";
 import listTpl from "./templateList.hbs";
 import cardTpl from "./templateCard.hbs";
@@ -34,4 +34,12 @@ function onInput(e) {
       });
     }
   });
+}
+function fetchCountries(e) {
+  const searchQuery = e.target.value;
+
+  const url = `https://restcountries.com/v2/name/${searchQuery}`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => data);
 }
